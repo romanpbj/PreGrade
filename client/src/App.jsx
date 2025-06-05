b4152dcdd3b7fa745add705c2e522c0efa3bad3b
unknown <romannn713@gmail.com>
2025-06-05 11:15:07 -0700

    frontend changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
+import axios from "axios";
+
 function App() {
+
+  const [file, setFile] = useState([]);
+
+  function handleChange(e){
+    setFile(e.target.files[0]);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (!file) return alert("Please upload a file first.");
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const res = await axios.post("http://localhost:5000/upload", formData);
+      alert("File uploaded: " + res.data.message);
+    } catch (err) {
+      console.error(err);
+      alert("Upload failed.");
+    }
+  }
+
+
   return (
     <div style={{ padding: "1rem" }}>
       <h2>PreGrade</h2>
-      <button onClick={() => alert("Analyze clicked")}>Analyze Assignment</button>
+      <form onSubmit={handleSubmit}>
+        <input type="file" onChange={handleChange} />
+        <button type="submit">Grade</button>
+      </form>
     </div>
   );
 }
+
 export default App;
