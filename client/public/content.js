a05f4ed5ef3931d885c8cdc6e541e83d95d50368
unknown <romannn713@gmail.com>
2025-06-05 13:57:47 -0700

    Fix content script and ignore secrets

--- client/public/content.js
+++ client/public/content.js
@@ -1,3 +1,5 @@
+console.log("Content script loaded");
+
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.type === "SCRAPE_TEXT") {
     const assignmentText = document.body.innerText.trim();
