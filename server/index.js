b43543a2599b556a501050875534f62e437e619c
TristanK117 <81729665+TristanK117@users.noreply.github.com>
2025-07-07 14:15:54 -0700

    test again

--- server/index.js
+++ server/index.js
@@ -180,15 +180,6 @@ app.post('/api/leniency', async (req, res) => {
   }
 });
 
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, 'client/build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
-
 app.listen(PORT, () => {
   console.log(` Server is running on http://localhost:${PORT}`);
 });
